f709d3f77856de15abcc6a7a7fcbf0894173b25b
Vishal Kumar <47864536+TizonX@users.noreply.github.com>
2020-10-03 00:54:41 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+const one=()=>
+    {
+        document.getElementsByClassName("one")[0].style.right="0px";
+    };
+    const two=()=>
+    {   
+        document.getElementsByClassName("two")[0].style.left="0px";
+    };
+    const three=()=>
+    {   
+        document.getElementsByClassName("three")[0].style.top="0px";
+    };
+    const four=()=>
+    {   
+        document.getElementsByClassName("four")[0].style.bottom="0px";
+    };
+    const reload=()=>
+    {
+        location.reload();
+    };
